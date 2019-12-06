3329e8ab0908b99d7339fbf3d0ed8e235caedeb5
seazx <seam622@163.com>
2019-12-06 17:36:34 +0800

    增加README.md，增加相应测试方法，注明线程安全的风险 --1.0.2

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # as-formula
 为Java精确计算提供更好的公式展示
+
+## 直接计算
+
+as-formula 可以对数学表达式直接进行精确计算，内部使用BigDecimal实现。
+
+如下算式，直接使用 0.1 + 0.2 会得到一个不准确的数值。
+
+```
+System.out.println(0.1 +0.2); // 0.30000000000000004
+Formula f = new Formula("0.1 + 0.2");
+System.out.println(f.calc().doubleValue()); // 0.3
+```
+
+通过使用Formula().calc()方法，会返回一个BigDecimal对象，  
+通过调用doubleValue()方法即可生成准确的double数值。  
+
+Formula对象会缓存上次的计算结果，多次调用可以提高计算效率。
+```
+Formula f = new Formula("0.1 + 0.2");
+System.out.println(f.calc().doubleValue()); // 0.3
+System.out.println(f.calc().doubleValue()); // 0.3
+```
+
+## 代数运算
+
+也可以输入代数，在执行时通过传入实际参数进行计算
+
+```
+Formula at = new Formula("(1 + i) ^ t");
+System.out.println(at.set(i, 0.0485).set(t, 360).calc()); // 1.2671912750046956
+```
+
+在第一次计算结束之后，可以只替换其中的部分参数进行计算
+
+```
+Formula at = new Formula("(1 + i) ^ t");
+System.out.println(at.set(i, 0.0485).set(t, 360).calc()); // 1.2671912750046956
+System.out.println(at.set("t", 3).calc()) // 1.152670834125
+```
+
+as-formula在多线程环境下会有线程不安全的问题(详见FormulaTest.threadTest方法)。所以暂时不建议将其设为公用静态变量。
--- src/main/test/expr/FormulaTest.java
+++ src/main/test/expr/FormulaTest.java
@@ -1,18 +1,65 @@
 package expr;
 
 import core.Formula;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 public class FormulaTest {
 
     @Test
-    public void test() {
-        Formula formula = new Formula("3 + 2 - 5 * 2");
-        System.out.println(formula.calc());
-        formula = new Formula("(3 + 2 - 5) * 2");
-        System.out.println(formula.calc());
-        Formula f = new Formula("a ^ 2 - b ^ 2").set("a", 5);
-        System.out.println(f.set("b", 4).calc());
-        System.out.println(f.set("b", 3).calc());
+    public void directTest() {
+        Assert.assertEquals(0.1 + 0.2, 0.30000000000000004, 0);
+        Formula f = new Formula("0.1 + 0.2");
+        Assert.assertEquals(f.calc().doubleValue(), 0.3, 0);
+        Assert.assertEquals(f.calc().doubleValue(), 0.3, 0);
     }
+
+    @Test
+    public void algebraTest() {
+        Formula at = new Formula("(1 + i) ^ t");
+        Assert.assertEquals(at.set("i", 0.0485).set("t", 5).calc().doubleValue(), 1.2671912750046956, 0);
+        Assert.assertEquals(at.set("t", 3).calc().doubleValue(), 1.152670834125, 0);
+    }
+
+    @Test
+    public void clearAndSetMapTest() {
+        Formula at = new Formula("(1 + i) ^ t");
+        Assert.assertEquals(at.set("i", 0.0485).set("t", 3).calc().doubleValue(), 1.152670834125, 0);
+        at.clear();
+        Map<String, Object> map = new HashMap<>();
+        map.put("i", 0.0485);
+        map.put("t", 5);
+        Assert.assertEquals(at.set(map).calc().doubleValue(), 1.26719127500469553125, 0);
+    }
+
+    @Test
+    public void threadTest() {
+        Random random = new Random();
+        Formula formula = new Formula("m * a");
+        CountDownLatch countDownLatch = new CountDownLatch(20);
+        for (int i = 0; i < 20; i++) {
+            Thread t = new Thread(() -> {
+                int m = random.nextInt(20);
+                int a = random.nextInt(20);
+                try {
+                    Assert.assertEquals(m * a, formula.set("m", m).set("a", a).calc().intValue());
+                } catch (Error e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+            t.start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
